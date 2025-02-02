aace3d8066c613575c1ea6764ac1de7a0037b187
Kieran Chambers <kieranc0808@gmail.com>
2025-02-02 01:52:57 +0000

    removed middleware

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,4 +1,11 @@
-export default function DashboardPage() {
+import { getSession } from "@/lib/get-session";
+import { redirect } from "next/navigation";
+
+export default async function DashboardPage() {
+    const session = await getSession();
+    const user = session?.user;
+    if (!user) redirect('/login');
+
     return (
         <h1>Dashboard</h1>
     );
