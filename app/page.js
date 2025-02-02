5bfcd1d42fa7dd4d5c3a3fa52c068b43baafdba6
Kieran Chambers <kieranc0808@gmail.com>
2025-02-02 14:20:57 +0000

    Update page.js

--- app/page.js
+++ app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
 				</div>
 				<div className="text-center">
 					<h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
-						Your personal reading haven on the web
+						Your Personal Reading Haven
 					</h1>
 					<p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-x1/8">
 						Discover a smarter way to manage your books, keep track of your favourites, and never lose sigh of what you've read - 
