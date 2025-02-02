4f2bf321e06dd22019126be89204113557501bff
Kieran Chambers <kieranc0808@gmail.com>
2025-02-02 16:02:33 +0000

    fixed google oauth register issue + added user details on dashboard

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,12 +1,21 @@
+import connectDB from "@/lib/db";
 import { getSession } from "@/lib/get-session";
+import { User } from "@/models/user";
 import { redirect } from "next/navigation";
 
 export default async function DashboardPage() {
     const session = await getSession();
     const user = session?.user;
     if (!user) redirect('/login');
+    await connectDB();
+    const userData = await User.findOne({ email: user.email });
 
     return (
-        <h1>Dashboard</h1>
+        <>
+            <h1>Dashboard</h1>
+            <p>{userData.email}</p>
+            <p>{userData.firstName + ' ' + userData.lastName}</p>
+            <p>{userData.username}</p>
+        </>
     );
 }
