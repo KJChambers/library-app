4f1144dae7bb21a2663a46c2f3bc7952c95e2713
Kieran Chambers <128253647+KJChambers@users.noreply.github.com>
2025-03-16 00:24:04 +0000

    minor adjustments

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,7 @@
 import Home from "@/app/(home)/page";
 import { render, screen } from "@testing-library/react";
 
-describe("Not Found Page", () => {
+describe("Home Page", () => {
 	it("renders a heading", () => {
 		render(<Home />);
 		const heading = screen.getByRole("heading", { level: 1 });
